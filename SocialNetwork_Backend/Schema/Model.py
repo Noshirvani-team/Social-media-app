1a416956528e9e561173790cf6a9792d6a9e0804
Sana Hejazi Rad <sanahejazirad@gmail.com>
2025-05-24 10:33:26 +0330

    Post,Like,Comments....Schema and Module Made

--- SocialNetwork_Backend/Schema/Model.py
+++ SocialNetwork_Backend/Schema/Model.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, Text, TIMESTAMP, func
 from DataBase.database import Base
+from sqlalchemy import PrimaryKeyConstraint
 
 
 class User(Base):
     __tablename__ = "Users"
-
     userid = Column(Integer, primary_key=True, index=True, autoincrement=True)
     username = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
@@ -12,3 +12,42 @@ class User(Base):
     profile_picture_link = Column(Text, nullable=True)
     created_at = Column(TIMESTAMP, server_default=func.now())
     is_private = Column(Boolean, default=False)
+
+class Post(Base):
+    __tablename__ = "Post"
+    postid = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    userid = Column(Integer, nullable=False)
+    description = Column(Text, nullable=True)
+    pic_video_link = Column(Text, nullable=True)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    is_published = Column(Boolean, default=True)
+
+class Comment(Base):
+    __tablename__ = "Comment"
+    commentid = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    userid = Column(Integer, nullable=False)
+    postid = Column(Integer, nullable=False)
+    context = Column(Text, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+class Like(Base):
+    __tablename__ = "Likes"
+    likeid = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    userid = Column(Integer, nullable=False)
+    postid = Column(Integer, nullable=False)
+
+class Notification(Base):
+    __tablename__ = "Notification"
+    notifid = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    userid = Column(Integer, nullable=False)
+    type = Column(String(100))
+    postlink = Column(Text, nullable=True)
+
+class Follower(Base):
+    __tablename__ = "Follower"
+    followerid = Column(Integer, primary_key=False)
+    followingid = Column(Integer, primary_key=False)
+    followed_at = Column(TIMESTAMP, server_default=func.now())
+    __table_args__ = (
+        PrimaryKeyConstraint('followerid', 'followingid'),
+    )
--- SocialNetwork_Backend/Schema/Schemas.py
+++ SocialNetwork_Backend/Schema/Schemas.py
@@ -1,25 +1,96 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel, EmailStr
 from typing import Optional
-
-
 
 try:
 
-   class UserRegister(BaseModel):
-     Username: str
-     Email: EmailStr
-     Password: str
-     Profile_Picture_Link: str | None = None
-     Is_Private:bool
+    #  User Schemas<<<<<>>>>>>
 
-   class UserLogin(BaseModel):
-       Email: EmailStr
-       Password: str
+    class UserRegister(BaseModel):
+        Username: str
+        Email: EmailStr
+        Password: str
+        Profile_Picture_Link: Optional[str] = None
+        Is_Private: bool
+
+    class UserLogin(BaseModel):
+        Email: EmailStr
+        Password: str
+
+    class Token(BaseModel):
+        access_token: str
+        token_type: str
+
+    # Post Schemas    <<<<<>>>>>
+
+    class PostCreate(BaseModel):
+        Description: Optional[str] = None
+        Pic_Video_Link: Optional[str] = None
+        Is_Published: Optional[bool] = True
+
+    class PostOut(BaseModel):
+        PostId: int
+        UserId: int
+        Description: Optional[str]
+        Pic_Video_Link: Optional[str]
+        Created_At: str
+        Is_Published: bool
+
+        class Config:
+            from_attributes = True
 
 
-   class Token(BaseModel):
-       access_token: str
-       token_type: str
+    #Comment Schemas <<<<<<<>>>>>>>
+
+    class CommentCreate(BaseModel):
+        PostId: int
+        Context: str
+
+    class CommentOut(BaseModel):
+        CommentId: int
+        UserId: int
+        PostId: int
+        Context: str
+        Created_At: str
+
+        class Config:
+            from_attributes = True
+
+    #Like Schemas<<<<<<<<>>>>>>>>
+
+    class LikeCreate(BaseModel):
+        PostId: int
+
+    class LikeOut(BaseModel):
+        LikeId: int
+        UserId: int
+        PostId: int
+
+        class Config:
+            from_attributes = True
+
+    #Notification Schemas <<<<<<<<< >>>>>>>>>
+
+    class NotificationOut(BaseModel):
+        NotifId: int
+        UserId: int
+        Type: Optional[str]
+        PostLink: Optional[str]
+
+        class Config:
+            from_attributes = True
+
+    #Follower Schemas <<<<<<<<>>>>>>>>
+
+    class FollowAction(BaseModel):
+        FollowingId: int
+
+    class FollowerOut(BaseModel):
+        FollowerId: int
+        FollowingId: int
+        Followed_At: str
+
+        class Config:
+            from_attributes = True
 
 except Exception as e:
-    print("Error :",e)
+    print("Error:", e)
